6aecf9d28e3e9b154feb425c7133c7fc4dc8ae7e
T. Nathan Mundhenk <mundhenk1@llnl.gov>
2020-01-24 16:40:24 -0800

    Removed demo.py and added auspices statement

--- conditional.py
+++ conditional.py
@@ -1,3 +1,47 @@
+'''
+BSD 3-Clause License
+
+Copyright (c) 2020, Lawrence Livermore National Laboratory
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+'''
+https://github.com/LLNL/fastcam
+
+A toolkit for efficent computation of saliency maps for explainable 
+AI attribution.
+
+This work was performed under the auspices of the U.S. Department of Energy 
+by Lawrence Livermore National Laboratory under Contract DE-AC52-07NA27344 
+and was supported by the LLNL-LDRD Program under Project 18-ERD-021 and 
+Project 17-SI-003. 
+
+Software released as LLNL-CODE-802426.
+'''
 from __future__ import print_function, division, absolute_import
 
 import torch
