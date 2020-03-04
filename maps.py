3a7c5b0580eeefe796e39e798ef7902ebe83b3c5
T. Nathan Mundhenk <mundhenk1@llnl.gov>
2020-03-03 16:57:27 -0800

    Init version stuff for BiDiCAM

--- maps.py
+++ maps.py
@@ -289,7 +289,41 @@ class Normalize2D(nn.Module):
         x       = x.reshape(s0,s1,s2)
             
         return x   
+    
+# ******************************************************************************************************************* 
+class GammaNorm2D(nn.Module):
+    r'''
+        This will normalize a saliency map to range from 0 to 1 via normal cumulative distribution function. 
+        
+        Input and output will be a 3D tensor of size [batch size x height x width]. 
+        
+        Input can be any real valued number (supported by hardware)
+        Output will range from 0 to 1
+    '''
+    
+    def __init__(self):
+        
+        super(GammaNorm2D, self).__init__()   
+        
+    def forward(self, x):
+        r'''
+            Original shape is something like [64,7,7] i.e. [batch size x height x width]
+        '''
+        assert(torch.is_tensor(x))
+        assert(len(x.size()) == 3) 
+        
+        s0      = x.size()[0]
+        s1      = x.size()[1]
+        s2      = x.size()[2]
 
+        x       = x.reshape(s0,s1*s2) 
+        
+
+                
+        x       = x.reshape(s0,s1,s2)
+            
+        return x  
+    
 # *******************************************************************************************************************     
 # *******************************************************************************************************************
 class CombineSaliencyMaps(nn.Module): 
@@ -388,3 +422,256 @@ class CombineSaliencyMaps(nn.Module):
         
         return cm, ww 
             
+# *******************************************************************************************************************
+# ******************************************************************************************************************* 
+class ScoreMap(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, scores):
+        
+        ctx.save_for_backward(scores)
+        return torch.tensor(1)
+    
+    @staticmethod
+    def backward(ctx, grad):
+                
+        saved        = ctx.saved_tensors
+        g_scores     = torch.ones_like(saved[0])
+        
+        return g_scores
+
+# *******************************************************************************************************************         
+class FastCAM(object):
+    r"""
+    Calculate SMOE Scale GradCAM salinecy map.
+    
+    This code is derived from pytorch-gradcam
+    """
+    def __init__(self, layers, model, method='V1', maps_method=maps.SMOEScaleMap, 
+                 use_gpp=False, grad_pooling=None, pos_neg_map=False):
+        
+        assert(isinstance(layers, list))
+        assert(isinstance(method, str))
+        assert(isinstance(grad_pooling, str) or grad_pooling is None)
+        assert(callable(maps_method))
+        
+        self.getSmap        = maps_method()
+        self.layers         = layers
+        self.model          = model
+        self.method         = method
+        self.use_gpp        = use_gpp
+        self.grad_pooling   = grad_pooling
+        self.pos_neg_map    = pos_neg_map
+        self.forward        = True
+        self.backward       = True
+        
+        if not pos_neg_map:
+            self.getNorm        = maps.Normalize2D()
+        else:
+            self.getNorm        = maps.Normalize2D()#const_mean=0.0)
+        
+        self.activation_hooks   = []
+        self.gradient_hooks     = []
+        
+        if self.method == 'V0':
+            self.backward = False
+        if self.method == 'V5':
+            self.forward = False
+            
+        if self.forward:
+            for i,l in enumerate(layers):
+                h   = misc.CaptureLayerOutput(post_process=None)
+                _   = self.model._modules[l].register_forward_hook(h)
+                self.activation_hooks.append(h)
+
+        if self.backward:    
+            for i,l in enumerate(layers):
+                if self.method=='V6' or self.method=='V5' or self.method=='V4' or self.method=='V3':
+                    h   = misc.CaptureGradInput(post_process=None) # The gradient information leaving the layer
+                else:
+                    h   = misc.CaptureGradOutput(post_process=None) # The gradient information entering the layer
+
+                _   = self.model._modules[l].register_backward_hook(h)
+                self.gradient_hooks.append(h)
+    
+    def _forward_bsize_1(self, class_idx, logit, retain_graph):
+        
+        if class_idx is None:
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            score = logit[:, class_idx].squeeze()
+
+        if self.backward:
+            self.model.zero_grad()
+            score.backward(retain_graph=retain_graph)
+    
+    def _forward_bsize_N(self):
+        
+        pass    
+    
+    def _magnitude_pool2d(self, x, kernel_size=2, stride=2, padding=0, pos_max=False):
+        
+        # pick the max magnitude gradient in the pool, the one with the highest absolute value
+
+        b, k, u, v          = x.size()
+
+        p1  = F.max_pool2d(x,    kernel_size=kernel_size, stride=stride, padding=padding, ceil_mode=True)
+        p2  = F.max_pool2d(-1*x, kernel_size=kernel_size, stride=stride, padding=padding, ceil_mode=True) * -1
+
+        d   = p1 + p2
+
+        if pos_max:
+            m   = torch.where(d >= 0.0, p1, torch.zeros_like(d))
+        else:
+            m   = torch.where(d >= 0.0, p1, p2)
+
+        m   = nn.functional.interpolate(m, size=[u,v], mode='nearest')        
+
+        return m 
+
+    def __call__(self, input, class_idx=None, retain_graph=False, invert=False):
+        """
+        Args:
+            input: input image with shape of (1, 3, H, W)
+            class_idx (int): class index for calculating GradCAM.
+                    If not specified, the class index that makes the highest model prediction score will be used.
+        Return:
+            mask: saliency map of the same spatial dimension with input
+            logit: model output
+        """
+
+        # Don't compute grads if we do not need them
+        with torch.set_grad_enabled(self.backward):
+
+            b, c, h, w      = input.size()
+    
+            self.model.eval()
+    
+            logit           = self.model(input)
+            
+            if b == 1:
+                self._forward_bsize_1(class_idx, logit, retain_graph)
+            else:
+                self._forward_bsize_N(class_idx, logit, retain_graph)
+            
+            saliency_maps = []
+            
+            for i,l in enumerate(self.layers):
+            
+                if self.backward:
+                    gradients           = self.gradient_hooks[i].data
+                    b, k, u, v          = gradients.size()
+                    
+                    # Make sure gradients are bounded on -1 to 1
+                    #if self.method=='V5' or self.method=='V4':
+                    #    sgrad               = torch.std(gradients) + 0.000001
+                    #    gradients           = gradients/(sgrad*(3.14159/2.0))
+                    #    gradients           = torch.tanh(gradients)
+                    
+                    if self.grad_pooling == 'avg':
+                        gradients           = F.avg_pool2d(gradients, kernel_size=2, stride=2, padding=0, ceil_mode=True)
+                        gradients           = nn.functional.interpolate(gradients, size=[u,v], mode='nearest') 
+                    elif self.grad_pooling == 'max':
+                        # this helps make gradients more selective for larger things when using V1, V2 or V3 
+                        # Should also remove spurioous negative gradients
+                        gradients           = F.max_pool2d(gradients, kernel_size=2, stride=2, padding=0, ceil_mode=True)
+                        gradients           = nn.functional.interpolate(gradients, size=[u,v], mode='nearest') 
+                    elif self.grad_pooling == 'mag':
+                        gradients           = self._magnitude_pool2d(gradients, kernel_size=2, stride=2, padding=0)
+                    
+                if self.forward:
+                    activations         = self.activation_hooks[i].data
+                    b, k, u, v          = activations.size()
+                    
+                    #sact                = torch.std(activations) + 0.000001
+                    #activations         = activations/sact
+                    #activations         = torch.tanh(activations)
+                    
+                    # Just to be sure, we may not be following a ReLU layer
+                    activations         = F.relu(activations)
+                
+                if invert:
+                    gradients *= -1
+                
+                # Do GradCAM Plus Plus preprocess on gradients?
+                # This biases for layers with less activation
+                # Only makes a noticable difference for V2 and V4
+                if self.use_gpp and self.backward and self.forward:
+                    alpha_num           = gradients.pow(2)
+                    alpha_denom         = gradients.pow(2).mul(2) + \
+                                            activations.mul(gradients.pow(3)).view(b, k, u*v).sum(-1, keepdim=True).view(b, k, 1, 1)
+                    # NOT NEEDED: since we already add 1e-7
+                    #alpha_denom         = torch.where(alpha_denom != 0.0, alpha_denom, torch.ones_like(alpha_denom))
+            
+                    alpha               = alpha_num.div(alpha_denom+1e-7)
+                    # OPTIONAL: Adding in the score is optional since we normalize the results any way
+                    #positive_gradients  = F.relu(self.score.exp()*gradients) # ReLU(dY/dA) == ReLU(exp(S)*dS/dA))
+                    positive_gradients  = F.relu(gradients)
+                    gradients           = alpha*positive_gradients
+                
+                if self.method=='V0':
+                    # Without Gradients, original SMOE Scale
+                    saliency_map        = activations
+                    
+                elif self.method=='V1':
+                    # V1 SIGN over layer means
+                    alpha               = gradients.view(b, k, -1).mean(2)
+                    weights             = alpha.view(b, k, 1, 1)
+                    weights             = torch.sign(weights) 
+                    saliency_map        = weights*activations
+                    
+                elif self.method=='V2':
+                    # V2 Original method
+                    alpha               = gradients.view(b, k, -1).mean(2)
+                    weights             = alpha.view(b, k, 1, 1)
+                    saliency_map        = weights*activations
+                    
+                elif self.method=='V3':
+                    # V3 SIGN over all values
+                    weights             = torch.sign(gradients) 
+                    saliency_map        = weights*activations
+                    
+                elif self.method=='V4':
+                    # V4 multiply over all values
+                    saliency_map        = gradients*activations
+                elif self.method=='V5':
+                    # V5 Gradients Only
+                    saliency_map        = gradients    
+                elif self.method=='V6':
+                    # V6 Sign of Gradients Only
+                    saliency_map        = torch.sign(gradients)
+
+                '''    
+                elif self.method=='V6':
+                    # Conditional entropy between postive and negative gradients 
+                    alpha               = gradients.view(b, k, -1).mean(2)
+                    weights_a           = F.relu(activations*alpha.view(b, k, 1, 1))    
+                    weights_b           = F.relu(activations*alpha.view(b, k, 1, 1) * -1)
+                    weights_a           = self.getSmap(weights_a)
+                    weights_b           = self.getSmap(weights_b)
+                    saliency_map        = self.getNorm(weights_a * torch.log(weights_a/weights_b))
+                    
+                    saliency_maps.append(saliency_map)
+                    
+                    continue
+                '''
+                    
+                if not self.pos_neg_map:
+                    # Only take the positive gradient values
+                    saliency_map        = F.relu(saliency_map)
+                    saliency_map        = self.getNorm(self.getSmap(saliency_map)).view(b, u, v)
+                else:
+                    smap1               = F.relu(saliency_map)
+                    smap2               = F.relu(saliency_map * -1)
+    
+                    smap1               = self.getSmap(smap1)
+                    smap2               = self.getSmap(smap2) * -1
+                    
+                    saliency_map        = smap1 + smap2
+                    saliency_map        = self.getNorm(saliency_map).view(b, u, v)
+                    
+                saliency_maps.append(saliency_map)
+            
+        return saliency_maps, logit  
+
+
